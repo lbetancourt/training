bfbf20482b6eb430ec067492d7309d3c356a146d
Luis Betancourt <luisber5330@gmail.com>
2021-10-11 18:14:18 -0500

    first commit

---
+++ src/test/java/com/lbetancourt/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbetancourt.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
